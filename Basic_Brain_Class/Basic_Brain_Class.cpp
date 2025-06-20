ba6078789259f5a2e2765c975e5aa729b9c7103e
junho1405 <junho1405@naver.com>
2025-06-20 15:42:07 +0900

    2025.06.20 0.2.1

--- Basic_Brain_Class/Basic_Brain_Class.cpp
+++ Basic_Brain_Class/Basic_Brain_Class.cpp
@@ -1,7 +1,12 @@
 ﻿#include<iostream>
 #include<ctime>
 #include <cstdlib>
+#include <chrono>
+#include <thread>
+#include <conio.h>
+
 using namespace std;
+using namespace chrono;
 
 class MiniGame
 {
@@ -12,7 +17,7 @@ public:
 };
 
 
-
+//덧셈영역
 class Plus_Quiz
 {
 	int score = 0;
@@ -99,9 +104,356 @@ public:
 	}
 
 };
+// 덧셈영역
+/// 뺄셈영역
+class Minus_Quiz
+{
+	int score = 0;
+	int quiz_count = 1;
+
+public:
+	void easy()
+	{
+		cout << "easy모드" << endl;
+		int score = 0;
+		while (quiz_count < 10)
+		{
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+			int answer;
+			cout << x << "-" << y << endl;
+			cin >> answer;
+			if (x - y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+				quiz_count++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+				quiz_count++;
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+	void nomal()
+	{
+		cout << "nomal모드" << endl;
+		int score = 0;
+		while (quiz_count < 10)
+		{
+			int x = rand() % 99 + 1;
+			int y = rand() % 99 + 1;
+			int answer;
+			cout << x << "-" << y << endl;
+			cin >> answer;
+			if (x - y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+				quiz_count++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+				quiz_count++;
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+	void hard()
+	{
+		cout << "hard모드" << endl;
+		int score = 0;
+		while (quiz_count < 10)
+		{
+			int x = rand() % 999 + 1;
+			int y = rand() % 999 + 1;
+			int answer;
+			cout << x << "-" << y << endl;
+			cin >> answer;
+			if (x - y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+				quiz_count++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+				quiz_count++;
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+
+};
+/// 뺄셈영역
+/// 곱셈영역
+class MultyPly_Quiz
+{
+	int score = 0;
+	int quiz_count = 1;
+
+public:
+	void easy()
+	{
+		cout << "easy모드" << endl;
+		int score = 0;
+		const int TimeLimit = 60;
+		auto StartTimeAttack = steady_clock::now();
+
+		while (true)
+		{
+
+			auto now = steady_clock::now();
+			auto elapsed = duration_cast<seconds>(now - StartTimeAttack).count();
+			cout << "[남은 시간: " << TimeLimit - elapsed << "초]" << endl;
+
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+			int answer;
+			cout << x << "*" << y << endl;
+			cin >> answer;
+			if (x * y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+				quiz_count++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+				quiz_count++;
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+	void nomal()
+	{
+		cout << "nomal모드" << endl;
+		int score = 0;
+		const int TimeLimit = 30;
+		auto StartTimeAttack = steady_clock::now();
+
+		while (true)
+		{
+
+			auto now = steady_clock::now();
+			auto elapsed = duration_cast<seconds>(now - StartTimeAttack).count();
+			cout << "[남은 시간: " << TimeLimit - elapsed << "초]" << endl;
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+			int answer;
+			cout << x << "*" << y << endl;
+
+			cin >> answer;
+			if (x * y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+				quiz_count++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+				quiz_count++;
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+	void hard()
+	{
+		cout << "hard모드" << endl;
+		int score = 0;
+		const int TimeLimit = 1;
+		auto StartTimeAttack = steady_clock::now();
+
+		while (true)
+		{
+			auto now = steady_clock::now();
+			auto elapsed = duration_cast<seconds>(now - StartTimeAttack).count();
+			int remaining = TimeLimit - elapsed;
+			if (remaining <= 0) break;  // 시간 초과 시 종료
+
+			cout << "[남은 시간: " << remaining << "초]" << endl;
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+			int answer;
+			cout << x << "*" << y << endl;
+
+			cin >> answer;
+			if (x * y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+				quiz_count++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+				quiz_count++;
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+
+};
+/// 곱셈영역
+class QuizMath 
+{
+public:
+	int SelectMath;
+	void math()
+	{
+
+		while (true)
+		{
+			cout << "종목을 선택해주세요" << endl;
+			cout << "덧셈[1] 뺄셈[2] 곱셈[3] 뒤로가기[0]" << endl;
+			int MathChoice;
+			cin >> MathChoice;
+
+			if (MathChoice == 0) break;
+			if (MathChoice == 1)
+			{
+				while (true)
+				{
+
+					cout << "난이도를 선택해주세요." << endl;
+					cout << "쉬움[1] 보통[2] 어려움[3] 뒤로가기[0]" << endl;
+					int Level;
+					cin >> Level;
+					
+					MiniGame;
+					if (Level == 0) break;
+					if (Level == 1)
+					{
+						Plus_Quiz Select_PQ;
+						Select_PQ.easy();
+					}
+					if (Level == 2)
+					{
+						Plus_Quiz Select_PQ;
+						Select_PQ.nomal();
+					}
+					if (Level == 3)
+					{
+						Plus_Quiz Select_PQ;
+						Select_PQ.hard();
+					}
+					
+					else
+					{
+						cout << "잘돗된 값을 입력했습니다." << endl;
+						break;
+					}
+				}
+			}
+			else if (MathChoice == 2)
+			{
+				while (true)
+				{
+					cout << "난이도를 선택해주세요." << endl;
+					cout << "쉬움[1] 보통[2] 어려움[3] 뒤로가기[0]" << endl;
+					int Level;
+					cin >> Level;
+					MiniGame;
+					if (Level == 0) break;
+					if (Level == 1)
+					{
+						Minus_Quiz Select_MQ;
+						Select_MQ.easy();
+					}
+					if (Level == 2)
+					{
+						Minus_Quiz Select_MQ;
+						Select_MQ.nomal();
+					}
+					if (Level == 3)
+					{
+						Minus_Quiz Select_MQ;
+						Select_MQ.hard();
+					}
+					else
+					{
+						cout << "잘돗된 값을 입력했습니다." << endl;
+					}
+				}
+			}
+			else if (MathChoice == 3)
+			{
+					while (true)
+				{
+					cout << "난이도를 선택해주세요." << endl;
+					cout << "쉬움[1] 보통[2] 어려움[3] 뒤로가기[0]" << endl;
+					int Level;
+					cin >> Level;
+					MiniGame;
+					if (Level == 0) break;
+					if (Level == 1)
+					{
+						MultyPly_Quiz Select_MQ;
+						Select_MQ.easy();
+					}
+					if (Level == 2)
+					{
+						MultyPly_Quiz Select_MQ;
+						Select_MQ.nomal();
+					}
+					if (Level == 3)
+					{
+						MultyPly_Quiz Select_MQ;
+						Select_MQ.hard();
+					}
+					else
+					{
+						cout << "잘돗된 값을 입력했습니다." << endl;
+					}
+				}
+			}
+			else
+			{
+				cout << "잘못된 값을 입력하셨습니다." << endl;
+			}
+		}
+	}
+};
+class ProgramStart
+{
+public:
+	void start()
+	{
+		while (true)
+		{
+			cout << "게임을 시작합니다." << endl;
+			cout << "[1]사칙연산 [2]기억력테스트 [3]반응속도테스트 [0]게임종료" << endl;
+			int SelectGame;
+			cin >> SelectGame;
+			if (SelectGame == 0) break;
+			if (SelectGame == 1)
+			{
+				QuizMath SelectMath;
+				SelectMath.math();
+			} 
+
+
+		}
+
+	}
+};
 
 //말랑말랑두뇌교실
 int main()
 {
+	ProgramStart ProgramStarter;
+	ProgramStarter.start();
 
 }
--- 지피티/지피티.cpp
+++ 지피티/지피티.cpp
@@ -1,76 +1,493 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <chrono>
+#include <conio.h>
+
 using namespace std;
+using namespace chrono;
 
-// 추상 클래스
-class MiniGame {
-protected:
-    int level;
+class MiniGame
+{
+private:
 public:
-    MiniGame(int lvl) : level(lvl) {}
-    virtual void play() = 0;
-    virtual ~MiniGame() {}
+	virtual void play() = 0;
+	virtual ~MiniGame() {}
 };
 
-// 덧셈 퀴즈 게임
-class PlusQuiz : public MiniGame {
+// 덧셈영역
+class Plus_Quiz
+{
 public:
-    PlusQuiz(int lvl) : MiniGame(lvl) {}
-    void play() override {
-        int max_num = (level == 1) ? 9 : (level == 2) ? 99 : 999;
-        int score = 0;
-        for (int i = 0; i < 10; i++) {
-            int x = rand() % max_num + 1;
-            int y = rand() % max_num + 1;
-            int answer;
-            cout << x << " + " << y << " = ";
-            cin >> answer;
-            if (answer == x + y) {
-                cout << "정답!\n";
-                score++;
-            }
-            else {
-                cout << "오답! 정답은 " << x + y << "\n";
-            }
-        }
-        cout << "총점수: " << score << "/10\n";
-    }
+	void easy()
+	{
+		cout << "easy모드" << endl;
+		int score = 0;
+		int quiz_count = 1;
+		while (quiz_count <= 9)
+		{
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+			int answer;
+			cout << x << " + " << y << " = ";
+			cin >> answer;
+			if (x + y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+			}
+			quiz_count++;
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count - 1 << endl;
+	}
+
+	void nomal()
+	{
+		cout << "nomal모드" << endl;
+		int score = 0;
+		int quiz_count = 1;
+		while (quiz_count <= 9)
+		{
+			int x = rand() % 99 + 1;
+			int y = rand() % 99 + 1;
+			int answer;
+			cout << x << " + " << y << " = ";
+			cin >> answer;
+			if (x + y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+			}
+			quiz_count++;
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count - 1 << endl;
+	}
+
+	void hard()
+	{
+		cout << "hard모드" << endl;
+		int score = 0;
+		int quiz_count = 1;
+		while (quiz_count <= 9)
+		{
+			int x = rand() % 999 + 1;
+			int y = rand() % 999 + 1;
+			int answer;
+			cout << x << " + " << y << " = ";
+			cin >> answer;
+			if (x + y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+			}
+			quiz_count++;
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count - 1 << endl;
+	}
 };
 
-// 게임 관리자: 게임 선택, 난이도 선택, 실행
-class GameManager {
+// 뺄셈영역
+class Minus_Quiz
+{
 public:
-    void run() {
-        while (true) {
-            cout << "\n게임을 선택하세요:\n1. 덧셈 퀴즈\n0. 종료\n선택: ";
-            int gameChoice;
-            cin >> gameChoice;
-            if (gameChoice == 0) break;
+	void easy()
+	{
+		cout << "easy모드" << endl;
+		int score = 0;
+		int quiz_count = 1;
+		while (quiz_count <= 9)
+		{
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+			int answer;
+			cout << x << " - " << y << " = ";
+			cin >> answer;
+			if (x - y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+			}
+			quiz_count++;
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count - 1 << endl;
+	}
 
-            cout << "난이도 선택 (1: 쉬움, 2: 보통, 3: 어려움): ";
-            int level; cin >> level;
+	void nomal()
+	{
+		cout << "nomal모드" << endl;
+		int score = 0;
+		int quiz_count = 1;
+		while (quiz_count <= 9)
+		{
+			int x = rand() % 99 + 1;
+			int y = rand() % 99 + 1;
+			int answer;
+			cout << x << " - " << y << " = ";
+			cin >> answer;
+			if (x - y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+			}
+			quiz_count++;
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count - 1 << endl;
+	}
 
-            MiniGame* game = nullptr;
-            if (gameChoice == 1) {
-                game = new PlusQuiz(level);
-            }
-
-            if (game) {
-                game->play();
-                delete game;
-            }
-            else {
-                cout << "잘못된 선택입니다.\n";
-            }
-        }
-        cout << "게임 종료!\n";
-    }
+	void hard()
+	{
+		cout << "hard모드" << endl;
+		int score = 0;
+		int quiz_count = 1;
+		while (quiz_count <= 9)
+		{
+			int x = rand() % 999 + 1;
+			int y = rand() % 999 + 1;
+			int answer;
+			cout << x << " - " << y << " = ";
+			cin >> answer;
+			if (x - y == answer)
+			{
+				cout << "정답입니다." << endl;
+				score++;
+			}
+			else
+			{
+				cout << "오답입니다." << endl;
+			}
+			quiz_count++;
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count - 1 << endl;
+	}
 };
 
-int main() {
-    srand(time(0));
-    GameManager gm;
-    gm.run();
-    return 0;
+// 곱셈영역 (시간 제한 있음)
+class MultyPly_Quiz
+{
+public:
+	void easy()
+	{
+		cout << "easy모드" << endl;
+		int score = 0;
+		int quiz_count = 0;
+		const int TimeLimit = 60; // 60초 제한
+		auto startTime = steady_clock::now();
+
+		while (true)
+		{
+			auto now = steady_clock::now();
+			auto elapsed = duration_cast<seconds>(now - startTime).count();
+			int remaining = TimeLimit - elapsed;
+			if (remaining <= 0)
+				break;
+
+			cout << "[남은 시간: " << remaining << "초]" << endl;
+
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+
+			cout << x << " * " << y << " = ";
+
+			// 입력 대기 + 시간 경과 체크
+			if (_kbhit())
+			{
+				int answer;
+				cin >> answer;
+				if (x * y == answer)
+				{
+					cout << "정답입니다." << endl;
+					score++;
+				}
+				else
+				{
+					cout << "오답입니다." << endl;
+				}
+				quiz_count++;
+			}
+			else
+			{
+				// 시간이 다 될 때까지 기다리지 말고 계속 루프 돌면서 남은 시간 체크
+				// sleep 조금 주면 CPU 과부하 줄일 수 있음
+				this_thread::sleep_for(milliseconds(100));
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+
+	void nomal()
+	{
+		cout << "nomal모드" << endl;
+		int score = 0;
+		int quiz_count = 0;
+		const int TimeLimit = 30; // 30초 제한
+		auto startTime = steady_clock::now();
+
+		while (true)
+		{
+			auto now = steady_clock::now();
+			auto elapsed = duration_cast<seconds>(now - startTime).count();
+			int remaining = TimeLimit - elapsed;
+			if (remaining <= 0)
+				break;
+
+			cout << "[남은 시간: " << remaining << "초]" << endl;
+
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+
+			cout << x << " * " << y << " = ";
+
+			if (_kbhit())
+			{
+				int answer;
+				cin >> answer;
+				if (x * y == answer)
+				{
+					cout << "정답입니다." << endl;
+					score++;
+				}
+				else
+				{
+					cout << "오답입니다." << endl;
+				}
+				quiz_count++;
+			}
+			else
+			{
+				this_thread::sleep_for(milliseconds(100));
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+
+	void hard()
+	{
+		cout << "hard모드" << endl;
+		int score = 0;
+		int quiz_count = 0;
+		const int TimeLimit = 15; // 15초 제한
+		auto startTime = steady_clock::now();
+
+		while (true)
+		{
+			auto now = steady_clock::now();
+			auto elapsed = duration_cast<seconds>(now - startTime).count();
+			int remaining = TimeLimit - elapsed;
+			if (remaining <= 0)
+				break;
+
+			cout << "[남은 시간: " << remaining << "초]" << endl;
+
+			int x = rand() % 9 + 1;
+			int y = rand() % 9 + 1;
+
+			cout << x << " * " << y << " = ";
+
+			if (_kbhit())
+			{
+				int answer;
+				cin >> answer;
+				if (x * y == answer)
+				{
+					cout << "정답입니다." << endl;
+					score++;
+				}
+				else
+				{
+					cout << "오답입니다." << endl;
+				}
+				quiz_count++;
+			}
+			else
+			{
+				this_thread::sleep_for(milliseconds(100));
+			}
+		}
+		cout << "총점수 : " << score << endl;
+		cout << "총문제수 : " << quiz_count << endl;
+	}
+};
+
+class QuizMath
+{
+public:
+	void math()
+	{
+		while (true)
+		{
+			cout << "종목을 선택해주세요" << endl;
+			cout << "덧셈[1] 뺄셈[2] 곱셈[3] 뒤로가기[0]" << endl;
+			int MathChoice;
+			cin >> MathChoice;
+
+			if (MathChoice == 0)
+				break;
+
+			if (MathChoice == 1)
+			{
+				while (true)
+				{
+					cout << "난이도를 선택해주세요." << endl;
+					cout << "쉬움[1] 보통[2] 어려움[3] 뒤로가기[0]" << endl;
+					int Level;
+					cin >> Level;
+
+					if (Level == 0)
+						break;
+					if (Level == 1)
+					{
+						Plus_Quiz pq;
+						pq.easy();
+					}
+					else if (Level == 2)
+					{
+						Plus_Quiz pq;
+						pq.nomal();
+					}
+					else if (Level == 3)
+					{
+						Plus_Quiz pq;
+						pq.hard();
+					}
+					else
+					{
+						cout << "잘못된 값을 입력했습니다." << endl;
+					}
+				}
+			}
+			else if (MathChoice == 2)
+			{
+				while (true)
+				{
+					cout << "난이도를 선택해주세요." << endl;
+					cout << "쉬움[1] 보통[2] 어려움[3] 뒤로가기[0]" << endl;
+					int Level;
+					cin >> Level;
+
+					if (Level == 0)
+						break;
+					if (Level == 1)
+					{
+						Minus_Quiz mq;
+						mq.easy();
+					}
+					else if (Level == 2)
+					{
+						Minus_Quiz mq;
+						mq.nomal();
+					}
+					else if (Level == 3)
+					{
+						Minus_Quiz mq;
+						mq.hard();
+					}
+					else
+					{
+						cout << "잘못된 값을 입력했습니다." << endl;
+					}
+				}
+			}
+			else if (MathChoice == 3)
+			{
+				while (true)
+				{
+					cout << "난이도를 선택해주세요." << endl;
+					cout << "쉬움[1] 보통[2] 어려움[3] 뒤로가기[0]" << endl;
+					int Level;
+					cin >> Level;
+
+					if (Level == 0)
+						break;
+					if (Level == 1)
+					{
+						MultyPly_Quiz mq;
+						mq.easy();
+					}
+					else if (Level == 2)
+					{
+						MultyPly_Quiz mq;
+						mq.nomal();
+					}
+					else if (Level == 3)
+					{
+						MultyPly_Quiz mq;
+						mq.hard();
+					}
+					else
+					{
+						cout << "잘못된 값을 입력했습니다." << endl;
+					}
+				}
+			}
+			else
+			{
+				cout << "잘못된 값을 입력하셨습니다." << endl;
+			}
+		}
+	}
+};
+
+class ProgramStart
+{
+public:
+	void start()
+	{
+		while (true)
+		{
+			cout << "게임을 시작합니다." << endl;
+			cout << "[1]사칙연산 [2]기억력테스트 [3]반응속도테스트 [0]게임종료" << endl;
+			int SelectGame;
+			cin >> SelectGame;
+			if (SelectGame == 0)
+				break;
+			if (SelectGame == 1)
+			{
+				QuizMath qmath;
+				qmath.math();
+			}
+			else
+			{
+				cout << "해당 게임은 아직 구현되지 않았습니다." << endl;
+			}
+		}
+	}
+};
+
+// 말랑말랑두뇌교실
+int main()
+{
+	srand(static_cast<unsigned int>(time(nullptr)));
+
+	ProgramStart ProgramStarter;
+	ProgramStarter.start();
+
+	return 0;
 }
